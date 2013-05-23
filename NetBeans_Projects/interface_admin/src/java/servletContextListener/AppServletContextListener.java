fb91b1b8f9ceef41812792df9376d9d3a345f503
HugoMuller <hugo.muller@free.fr>
2013-05-23 11:21:53 +0200

    connexion/creation de la db au deploiement

--- NetBeans_Projects/interface_admin/src/java/servletContextListener/AppServletContextListener.java
+++ NetBeans_Projects/interface_admin/src/java/servletContextListener/AppServletContextListener.java
@@ -1,8 +1,18 @@
 package servletContextListener;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//import java.sql.SQLWarning;
+import java.sql.Statement;
+import java.util.ArrayList;
+//import java.util.Properties;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import org.apache.derby.tools.ij;
+//import org.apache.derby.tools.ij;
+
 /**
  *
  * @author Hugo
@@ -10,16 +20,144 @@ import org.apache.derby.tools.ij;
 
 public class AppServletContextListener implements ServletContextListener
 {
+    private String framework = "derbyclient"; //"embedded";
+    private String driver = "org.apache.derby.jdbc.ClientDriver"; //"org.apache.derby.jdbc.EmbeddedDriver";
+    private String protocol = "jdbc:derby://localhost:1527/"; //"jdbc:derby:";
+    private String DBname = "EcologU_DB";
+    
     @Override
     public void contextInitialized(ServletContextEvent arg0)
     {
         System.out.println("ServletContextListener started");
-        
+        this.setUpDB();
     }
     
     @Override
     public void contextDestroyed(ServletContextEvent arg0)
     {
         System.out.println("ServletContextListener destroyed");
+    }
+    
+    private void setUpDB()
+    {
+        this.loadDriver();
+        Connection con = null;
+        ArrayList<Statement> statements = new ArrayList<>();
+        Statement s = null;
+
+        try
+        {
+            //uncomment to add a user and an authenticated connection
+            //Properties props = new properties();
+            //props.put("user", "root");
+            //props.put("password", "root");
+            con = DriverManager.getConnection(protocol + DBname + ";create=true;");
+            con.setAutoCommit(false);
+            s = con.createStatement();
+            statements.add(s);
+            s.execute("CREATE TABLE heating("
+                        + "date date not null primary key,"
+                        + "consommation double"
+                        + ");");
+            System.out.println("Création de la table 'heating'");
+
+            s.execute("CREATE TABLE light("
+                        + "date date not null primary key,"
+                        + "consommation double"
+                        + ");");
+            System.out.println("Création de la table 'light'");
+            con.commit();
+            System.out.println("Transactions");
+        }
+        catch(SQLException sqle)
+        {
+            printSQLException(sqle);
+            if("01J01".equals(sqle.getSQLState()))
+            {
+                System.out.println(sqle.getMessage());
+            }
+            else
+            {
+                System.out.println("Création de la DB " + DBname);
+                System.out.println("Connection à la DB " + DBname);
+            }
+        }
+        finally
+        {
+            while(!statements.isEmpty())
+            {
+                Statement st = (Statement) statements.remove(0);
+                try
+                {
+                    if(st != null)
+                    {
+                        st.close();
+                        st = null;
+                    }
+                }
+                catch(SQLException sqle)
+                {
+                    printSQLException(sqle);
+                }
+            }
+            
+            try
+            {
+                if (con != null)
+                {
+                    con.close();
+                    con = null;
+                }
+            }
+            catch(SQLException sqle)
+            {
+                printSQLException(sqle);
+            }
+        }
+    }
+    
+    private void loadDriver()
+    {
+        try
+        {
+            Class.forName(driver).newInstance();
+            System.out.println("Loaded the appropriate driver");
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+            System.err.println("\nUnable to load the JDBC driver " + driver);
+            System.err.println("Please check your CLASSPATH.");
+            cnfe.printStackTrace(System.err);
+        }
+        catch(InstantiationException ie)
+        {
+            System.err.println("\nUnable to instantiate the JDBC driver " + driver);
+            ie.printStackTrace(System.err);
+        }
+        catch(IllegalAccessException iae)
+        {
+            System.err.println("\nNot allowed to access the JDBC driver " + driver);
+            iae.printStackTrace(System.err);
+        }
+    }
+    
+    private void reportFailure(String message)
+    {
+        System.err.println("\nData verification failed:");
+        System.err.println('\t' + message);
+    }
+    
+    public static void printSQLException(SQLException e)
+    {
+        while(e != null)
+        {
+            System.err.println("\n----- SQLException -----");
+            System.err.println("  SQL State:  " + e.getSQLState());
+            System.err.println("  Error Code: " + e.getErrorCode());
+            System.err.println("  Message:    " + e.getMessage());
+            // for stack traces, refer to derby.log or uncomment this:
+            //e.printStackTrace(System.err);
+            e = e.getNextException();
+        }
     }
 }
